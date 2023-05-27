48618c19f2f5a6a8d4aaf7ec0066a55396d4ecd0
tugbatoyoglu <92046533+tugbatoyoglu@users.noreply.github.com>
2023-05-27 11:58:54 +0300

    kafeler yapıldı

--- KafePusulam/lepetitcafe.php
+++ KafePusulam/lepetitcafe.php
@@ -6,7 +6,7 @@ include('baglanti.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Formdan gelen verileri alın
     $yildiz = $_POST['yildiz'][0]; // İlk seçilen yıldız değerini alın
-    $yorum = $_POST['yorum'];
+    $yorum = $_POST['mesaj'];
 
     // Kullanıcı adını 'crudseries' tablosundan çekin
     $sql = "SELECT kullanici_adi FROM seriescrud ORDER BY id DESC LIMIT 1";
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         // Yorumu veritabanına kaydedin
         $tarih = date("Y-m-d H:i:s");
-        $insertSql = "INSERT INTO yorumaurora  (kullanici, yorum, yildiz, tarih) VALUES ('$kullanici', '$yorum', '$yildiz', '$tarih')";
+        $insertSql = "INSERT INTO yorumlepetitcafe (kullanici, yorum, yildiz, tarih) VALUES ('$kullanici', '$yorum', '$yildiz', '$tarih')";
         if ($baglanti->query($insertSql) === TRUE) {
             echo "Yorum başarıyla kaydedildi.";
         } else {
@@ -31,18 +31,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 // Yorumları veritabanından çekin
-$selectSql = "SELECT * FROM yorumaurora ";
+$selectSql = "SELECT * FROM yorumlepetitcafe";
 $result = $baglanti->query($selectSql);
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aurora Delights</title>
+    <title>Le Petit Café</title>
     <link rel="stylesheet" href="styles/kafe.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
@@ -50,56 +48,56 @@ $result = $baglanti->query($selectSql);
     <div class="wrapper">
         <div class="container">
             <div class="left">
-                <img src="images/tan-kafe6.svg" alt="Aurora Delights">
+                <img src="images/tan-kafe2.svg" alt="Le Petit Café">
             </div>
             <div class="right">
-                <h1>Aurora Delights</h1>
-                <p class="tanitim">"Aurora Delights", alkolsüz bir ortamda Dünya Mutfağının lezzetlerini sunan ve bütçe dostu bir kafe olarak karşımıza çıkıyor. Bu benzersiz mekan, farklı kültürlerin enfes tatlarını bir araya getirirken, sağlıklı ve besleyici seçenekleriyle de dikkat çekiyor. Aurora Delightsta, çeşitli lezzetler arasından seçim yaparken cüzdanınızı da düşünebilirsiniz. Sıcak ve davetkar atmosferiyle Aurora Delights, uygun fiyatlarla keyifli bir yemek deneyimi sunuyor.</p>
+                <h1>Le Petit Café</h1>
+                <p class="tanitim">Le Petit Café, alkollü içeceklerin keyfini çıkarmak isteyenler, canlı müzikle ruhunu şenlendirmek isteyenler ve yeni nesil kahveci deneyimi arayanlar için ideal bir mekan. Bu eşsiz kafe, zengin içecek seçenekleriyle ve ferah atmosferiyle misafirlerini karşılıyor. Lezzetli kokteyller, seçkin bira ve şaraplarla içkinizi yudumlarken, canlı müzik performansları size unutulmaz anlar yaşatacak. Le Petit Café, modern bir kahveci konseptiyle birleşen eğlenceli ve samimi bir ortam sunarak sizi sıradışı bir deneyime davet ediyor.</p>
 
                 <div class="bottom">
-                <div class="bottom-left">
-                        <p class="konum"><img src="images/konum_ikon.svg" alt="konumikonu"> Kadıköy/İstanbul</p>
+                    <div class="bottom-left">
+                        <p class="konum"><img src="images/konum_ikon.svg" alt="konumikonu"> Bakırköy/İstanbul</p>
                         
                         <p class="telefon"><img src="images/telefon.svg" alt="konumikonu"> 444 44 44</p>
-                        <div class="btn-field">
-                    <button class="menüyügör" id="myBtn">Menüyü Gör</button>
-                 </div>
 
-                    <!-- The Modal -->
-                    <div id="myModal" class="modal">
-                    
-                      <!-- Modal content -->
-                      <div class="modal-content1">
-                        <span class="close">&times;</span>
-                        <img src="images/menü.svg" alt="Menü">
-                      </div>
-                    
+                        <div class="btn-field">
+                        <button class="menüyügör" id="myBtn">Menüyü Gör</button>
+                     </div>
+
+                        <!-- The Modal -->
+                        <div id="myModal" class="modal">
+                        
+                          <!-- Modal content -->
+                          <div class="modal-content1">
+                            <span class="close">&times;</span>
+                            <img src="images/menü.svg" alt="Menü">
+                          </div>
+                        
+                        </div>
+                        
+                    </div>
+
+                    <div class="bottom-right">
+                        <p class="mail"><img src="images/mail_icon.svg" alt="mailikonu"> kafepusulam@gmail.com</p>
+
+                        <p class="website"><img src="images/website.svg" alt="bilgisayarikonu"> www.lorem.com</p>
+
+                       <div class="btn-field2">
+                        <button class="degerlendir" id="myBtn2">Değerlendir</button>
                     </div>
                     
-                </div>
+                        <div id="myModal2" class="modal2">
+                          <div class="modal-content2">
+                            <span class="close2">&times;</span>
+                            <h2>Le Petit Café</h2>
 
-                <div class="bottom-right">
-                    <p class="mail"><img src="images/mail_icon.svg" alt="mailikonu"> kafepusulam@gmail.com</p>
+                            <div class="container d-flex justify-content-center mt-200">
+                                <div class="row">
 
-                    <p class="website"><img src="images/website.svg" alt="bilgisayarikonu"> www.lorem.com</p>
-
-                   <div class="btn-field2">
-                    <button class="degerlendir" id="myBtn2">Değerlendir</button>
-                </div>
-                
-                    <div id="myModal2" class="modal2">
-                      <div class="modal-content2">
-                        <span class="close2">&times;</span>
-                        <h2>Aurora Delights</h2>
-
-                        <div class="container d-flex justify-content-center mt-200">
-                            <div class="row">
-
-                                <div class="col-md-12">
-                                    <div class="stars">
-                            
-                              <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-                            
+                                    <div class="col-md-12">
+                                        <div class="stars">
+                                        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+                                
                                 <input class="star star-5" id="star-5" type="radio" name="yildiz[]" value="5"/>
                                 <label class="star star-5" for="star-5"></label>
                             
@@ -119,45 +117,40 @@ $result = $baglanti->query($selectSql);
                                 <input class="star star-1" id="star-1" type="radio" name="yildiz[]" value="1"/>
                                 <label class="star star-1" for="star-1"></label>
                             
-                             
-
-                        </div>
-                           
-                                    </div>
-
-                        </div>
-
-                        </div>
-                     
+                              
                             
-                                <div class="mesaj">
-                                    <label for="mesaj"></label>
-                                    <input type="text" name="yorum" placeholder="Düşüncelerinizi İletin.">
-                                </div>
-            
-                                <div class="btn-field3">
-                                    <button class="btn" type="submit">Değerlendir</button>
-                                </div>
-                     </form>
-                            
-                            
+                            </div>
                             
                             
                                     
+                                </div>
                                 
-                                
                             
+                            </div>
                             
-                            
-                            
-                        
-                      </div>
-                    
-                    </div>
-                </div>
-            </div>
+                            </div>
 
-            <div class="yorumlar">
+                        
+                            <div class="mesaj">
+                                <label for="mesaj"></label>
+                                <input type="text" name="mesaj" placeholder="Düşüncelerinizi İletin.">
+                            </div>
+            
+                            <div class="btn-field3">
+                                <button class="btn" type="submit">Değerlendir</button>
+                            </div>
+                        </form>
+                            
+                            
+                          </div>
+                        
+                        </div>
+                    </div>
+
+                   
+                </div>
+
+                <div class="yorumlar">
     <?php while ($row = $result->fetch_assoc()) { ?>
         <div class="yorum">
             <div class="left">
@@ -186,46 +179,55 @@ $result = $baglanti->query($selectSql);
         </div>
     <?php } ?>
 </div>
+                </div>
+  
+
+
+                </div>
+
+                
+
+            </div>
 
         </div>
     </div>
-</div>
-</div>
-</div>
-<script>
+
+    <script>
         var modal = document.getElementById("myModal");
         var btn = document.getElementById("myBtn");
         var span = document.getElementsByClassName("close")[0];
         btn.onclick = function() {
           modal.style.display = "block";
         }
+        
         span.onclick = function() {
-      modal.style.display = "none";
-    }
-    
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
+          modal.style.display = "none";
+        }
+        
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
 
 
 
 
-     var modal2 = document.getElementById("myModal2");
-     var btn2 = document.getElementById("myBtn2");
-     var span2 = document.getElementsByClassName("close2")[0]; 
-    btn2.onclick = function() {
-      modal2.style.display = "block";
-    }
-    span2.onclick = function() {
-      modal2.style.display = "none";
-    }
-    window.onclick = function(event) {
-      if (event.target == modal2) {
-        modal2.style.display = "none";
-      }
-    }
-    </script>
+         var modal2 = document.getElementById("myModal2");
+         var btn2 = document.getElementById("myBtn2");
+         var span2 = document.getElementsByClassName("close2")[0]; 
+        btn2.onclick = function() {
+          modal2.style.display = "block";
+        }
+        span2.onclick = function() {
+          modal2.style.display = "none";
+        }
+        window.onclick = function(event) {
+          if (event.target == modal2) {
+            modal2.style.display = "none";
+          }
+        }
+        </script>
+        
 </body>
 </html>
